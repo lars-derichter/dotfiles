cf42494d84199a4135e36fe025da44f386b9e0b3
Lars De Richter <lars.derichter@gmail.com>
2020-08-11 02:08:21 +0200

    Add gpg
    - add gpg-configuration
    - import my private key from 1password
    - always sign git commits with gpg

---
+++ private_dot_init/run_once_add-gpg-agent.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+/usr/local/bin/gpg --import ~/.gnupg/private.key
+expect -c "spawn gpg --edit-key 472D6B4D6F760DD88F480E54306A874F98AFF064 trust quit; send \"5\ry\r\"; expect eof"
+rm ~/.gnupg/private.key
+
+echo "Check if key is shown with ultimate trust and correct identity"
+/usr/local/bin/gpg --list-keys
+
+# (re)start gpg-agent
+killall gpg-agent
+gpg-agent --daemon
