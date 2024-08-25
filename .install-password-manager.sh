b5ac018278be57a19289d8ff8573e9795f7a9f77
Lars De Richter <lars.derichter@gmail.com>
2024-08-25 22:41:23 +0200

    Trying to fix 1password and brews

---
+++ .install-password-manager.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# exit immediately if password-manager-binary is already in $PATH
+type password-manager-binary >/dev/null 2>&1 && exit
+
+case "$(uname -s)" in
+Darwin)
+    # commands to install password-manager-binary on Darwin
+    brew install 1password-cli
+    op account add --address my.1password.com --email lars.derichter@gmail.com
+    eval $(op signin)
+    ;;
+Linux)
+    # commands to install password-manager-binary on Linux
+    echo "No password manager for Linux selected"
+    ;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
